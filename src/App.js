e8201862e94f342865466e996f3ea657b6dd427a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-20 10:21:05 +0100

    setup App.js functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import { data } from "./data"
+import Split from "react-split"
+import { nanoid } from "nanoid"
+
+/**
+ * Challenge: Spend 10-20+ minutes reading through the code
+ * and trying to understand how it's currently working. Spend
+ * as much time as you need to feel confident that you 
+ * understand the existing code (although you don't need
+ * to fully understand everything to move on)
+ */
 
 export default function App() {
+    const [notes, setNotes] = useState([])
+    const [currentNoteId, setCurrentNoteId] = useState((notes[0] && notes.id) || "")
+
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+
+    function updateNote(text) {
+        setNotes(oldNotes => oldNotes.map(oldNote => {
+            return oldNote.id === currentNoteId
+                ? { ...oldNote, body: text }
+                : oldNote
+        }))
+    }
+
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+
     return (
-        <h1>Hello from the other side</h1>
+        <h1>Hello from the other site</h1>
     )
 }
