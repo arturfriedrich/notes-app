b564c26df912497230ea6eb31117bb3e60f90e18
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-20 11:53:11 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import { data } from "./data"
 import Split from "react-split"
 import { nanoid } from "nanoid"
 
-/**
- * Challenge: Spend 10-20+ minutes reading through the code
- * and trying to understand how it's currently working. Spend
- * as much time as you need to feel confident that you 
- * understand the existing code (although you don't need
- * to fully understand everything to move on)
- */
-
 export default function App() {
     const [notes, setNotes] = useState([])
     const [currentNoteId, setCurrentNoteId] = useState(
