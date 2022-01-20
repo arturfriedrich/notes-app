21184bebf4bca7bc968c8fdf687096f14d840dc7
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-20 10:26:14 +0100

    update data.js

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        id: 1,
+        body: `### This just in!
+        
+Markdown is an *awesome* way to write **very basic html** by typing manually
+
+It can be used for:
+
+1. README files
+2. Slack and Discord messages
+3. **So much more**
+
+---
+
+Here's a division`,
+    },
+    { id: 2, body: `a` },
+    { id: 3, body: `b` },
+    { id: 4, body: `c` },
+    { id: 5, body: `d` },
+    { id: 6, body: `e` },
+]
