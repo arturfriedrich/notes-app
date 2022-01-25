5d6dbcaedb900b631cc98376557d0c76ece07b0f
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-25 15:07:16 +0100

    Added functionality to delete notes

--- src/App.js
+++ src/App.js
@@ -44,6 +44,11 @@ export default function App() {
         })
     }
 
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes(oldNotes => oldNotes.filter(note => (note.id !== noteId)))
+    }
+
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
@@ -65,6 +70,7 @@ export default function App() {
                             currentNote={findCurrentNote()}
                             setCurrentNoteId={setCurrentNoteId}
                             newNote={createNewNote}
+                            deleteNote={deleteNote}
                         />
                         {
                             currentNoteId &&
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,6 +10,13 @@ export default function Sidebar(props) {
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{(note.body.split("\n"))[0]}</h4>
+                <button
+                    className="delete-btn"
+                    // Your onClick event handler here
+                    onClick={(event) => props.deleteNote(event, note.id)}
+                >
+                    <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
     ))
